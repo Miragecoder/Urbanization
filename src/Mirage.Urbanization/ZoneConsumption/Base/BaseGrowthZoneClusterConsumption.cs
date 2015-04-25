e19f7e9ae29103f11cfd4de807ad2d521ee6e83c
Robbert Wijkstra <Miragecoder@users.noreply.github.com>
2015-04-25 12:10:51 +0200

    Clean up 'BaseGrowthZoneClusterConsumption'.

--- src/Mirage.Urbanization/ZoneConsumption/Base/BaseGrowthZoneClusterConsumption.cs
+++ src/Mirage.Urbanization/ZoneConsumption/Base/BaseGrowthZoneClusterConsumption.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq;
 using System.Drawing;
 using Mirage.Urbanization.ZoneConsumption.Base.Behaviours;
 
@@ -13,15 +9,21 @@ namespace Mirage.Urbanization.ZoneConsumption.Base
         void Set(int id, int population);
     }
 
-    public abstract class BaseGrowthZoneClusterConsumption : BaseImplementedZoneClusterConsumption, IAreaObjectWithSeed, IBaseGrowthZoneClusterConsumptionInLoadContext
+    public abstract class BaseGrowthZoneClusterConsumption : BaseImplementedZoneClusterConsumption, IAreaObjectWithSeed,
+        IBaseGrowthZoneClusterConsumptionInLoadContext
     {
-        private int _id = Random.Next(0, Int32.MaxValue);
-
-        public int Id { get { return _id; } }
-
         private static readonly Random Random = new Random();
-
+        private readonly ICrimeBehaviour _crimeBehaviour;
         private readonly IPollutionBehaviour _pollutionBehaviour;
+        private int _id = Random.Next(0, int.MaxValue);
+
+        protected BaseGrowthZoneClusterConsumption(Func<ZoneInfoFinder> createZoneInfoFinderFunc, Color color)
+            : base(createZoneInfoFinderFunc, new ElectricityConsumerBehaviour(15), color, 3, 3)
+        {
+            _pollutionBehaviour =
+                new DynamicPollutionBehaviour(() => Convert.ToInt32(PopulationDensity*PopulationPollutionFactor));
+            _crimeBehaviour = new DynamicCrimeBehaviour(() => Convert.ToInt32(PopulationDensity*PopulationCrimeFactor));
+        }
 
         public override IPollutionBehaviour PollutionBehaviour
         {
@@ -33,38 +35,51 @@ namespace Mirage.Urbanization.ZoneConsumption.Base
             get { return _crimeBehaviour; }
         }
 
-        private readonly ICrimeBehaviour _crimeBehaviour;
-
-        protected BaseGrowthZoneClusterConsumption(Func<ZoneInfoFinder> createZoneInfoFinderFunc, Color color)
-            : base(createZoneInfoFinderFunc, new ElectricityConsumerBehaviour(15), color, 3, 3)
-        {
-            _pollutionBehaviour = new DynamicPollutionBehaviour(() => Convert.ToInt32(_populationDensity * PopulationPollutionFactor));
-            _crimeBehaviour = new DynamicCrimeBehaviour(() => Convert.ToInt32(_populationDensity * PopulationCrimeFactor));
-        }
-
         protected abstract decimal PopulationPollutionFactor { get; }
         protected abstract decimal PopulationCrimeFactor { get; }
+        public int PopulationDensity { get; private set; }
 
-        private int _populationDensity;
+        public int PopulationStatistics
+        {
+            get { return PopulationDensity*3; }
+        }
 
-        public int PopulationDensity { get { return _populationDensity; } }
+        public bool CanGrow
+        {
+            get { return PopulationDensity < 50; }
+        }
 
-        public int PopulationStatistics { get { return _populationDensity * 3;} }
+        public bool CanGrowAndHasPower
+        {
+            get { return CanGrow && HasPower; }
+        }
 
-        public bool CanGrow { get { return _populationDensity < 50; } }
+        public bool IsPopulated
+        {
+            get { return PopulationDensity > 0; }
+        }
 
-        public bool CanGrowAndHasPower { get { return CanGrow && HasPower; } }
+        public int AverageTravelDistance { get; private set; }
 
-        public bool IsPopulated { get { return _populationDensity > 0; } }
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        void IBaseGrowthZoneClusterConsumptionInLoadContext.Set(int id, int populationDensity)
+        {
+            _id = id;
+            PopulationDensity = populationDensity;
+        }
 
         public void IncreasePopulation()
         {
-            if (CanGrow) _populationDensity++;
+            if (CanGrow) PopulationDensity++;
         }
 
         public void IncreaseOrDecreaseByPoweredState()
         {
-            if (this.HasPower)
+            if (HasPower)
                 IncreasePopulation();
             else
                 DecreasePopulation();
@@ -72,22 +87,12 @@ namespace Mirage.Urbanization.ZoneConsumption.Base
 
         public void DecreasePopulation()
         {
-            if (_populationDensity > 0) _populationDensity--;
+            if (PopulationDensity > 0) PopulationDensity--;
         }
-
-        private int _averageTravelDistance;
-
-        public int AverageTravelDistance { get { return _averageTravelDistance; } }
 
         public void SetAverageTravelDistance(int distance)
         {
-            _averageTravelDistance = distance;
-        }
-
-        void IBaseGrowthZoneClusterConsumptionInLoadContext.Set(int id, int populationDensity)
-        {
-            _id = id;
-            _populationDensity = populationDensity;
+            AverageTravelDistance = distance;
         }
     }
 }
