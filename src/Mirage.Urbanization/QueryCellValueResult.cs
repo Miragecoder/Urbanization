c320276f6e0726c7cd6cf6c380711072a2496a77
Robbert Wijkstra <Miragecoder@users.noreply.github.com>
2015-07-07 18:07:00 +0200

    Add the missing 'QueryCellValueResult'-file.

---
+++ src/Mirage.Urbanization/QueryCellValueResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing.Text;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+namespace Mirage.Urbanization
+{
+    public abstract class QueryCellValueResult : IQueryCellValueResult
+    {
+        private readonly int _valueInUnits;
+        protected QueryCellValueResult(int valueInUnits)
+        {
+            _valueInUnits = valueInUnits > 0 ? valueInUnits : 0;
+        }
+        public int ValueInUnits { get { return _valueInUnits; } }
+    }
+}
