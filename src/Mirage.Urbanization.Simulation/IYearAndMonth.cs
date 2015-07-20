181b893947dcae6142feef3e744853eff64a0971
Robbert Wijkstra <Miragecoder@users.noreply.github.com>
2015-07-20 22:12:08 +0200

    Move 'IReadOnlyYearAndMonth' into a seperate file.

--- src/Mirage.Urbanization.Simulation/IYearAndMonth.cs
+++ src/Mirage.Urbanization.Simulation/IYearAndMonth.cs
@@ -2,13 +2,6 @@
 
 namespace Mirage.Urbanization.Simulation
 {
-    public interface IReadOnlyYearAndMonth
-    {
-        bool IsAtBeginningOfNewYear { get; }
-        string GetCurrentDescription();
-        int CurrentYear { get; }
-    }
-
     public interface IYearAndMonth : IReadOnlyYearAndMonth
     {
         event EventHandler<YearAndMonthWeekElapsedEventArgs> OnWeekElapsed;
