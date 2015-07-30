a92c14f1f14c6cd10411c9c8e48a8aff1a5f88b7
Robbert Wijkstra <Miragecoder@users.noreply.github.com>
2015-07-30 20:22:02 +0200

    Add 'RandomTerraformerTest'.

---
+++ src/Mirage.Urbanization.Test/RandomTerraformerTest.cs
@@ -0,0 +1,66 @@
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mirage.Urbanization.ZoneConsumption;
+using Mirage.Urbanization.ZoneConsumption.Base;
+using Mirage.Urbanization.ZoneStatisticsQuerying;
+using Rhino.Mocks;
+
+namespace Mirage.Urbanization.Test
+{
+    [TestClass]
+    public class RandomTerraformerTest
+    {
+        private ZoneInfoGrid InitializeZoneInfoGrid(bool withHorizontalRiver, bool withVerticalRiver, bool withWoodlands)
+        {
+            var r = new RandomTerraformer(
+                () => new WaterZoneConsumption(new ZoneInfoFinder(x => new QueryResult<IZoneInfo>())),
+                () => new WoodlandZoneConsumption(new ZoneInfoFinder(x => new QueryResult<IZoneInfo>())));
+            var zoneInfoGrid = new ZoneInfoGrid(100, MockRepository.GenerateMock<ILandValueCalculator>());
+
+            var options = new TerraformingOptions()
+            {
+                HorizontalRiver = withHorizontalRiver,
+                VerticalRiver = withVerticalRiver
+            };
+
+            options.SetWoodlands(withWoodlands ? 10 : 0);
+
+            r.ApplyWith(zoneInfoGrid, options);
+
+            return zoneInfoGrid;
+        }
+
+        [TestMethod]
+        public void TestNoTerraforming()
+        {
+            var zoneInfoGrid = InitializeZoneInfoGrid(withHorizontalRiver: false, withVerticalRiver: false, withWoodlands: false);
+
+            Assert.IsTrue(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is EmptyZoneConsumption));
+            Assert.IsFalse(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is WoodlandZoneConsumption));
+            Assert.IsFalse(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is WaterZoneConsumption));
+        }
+
+        [TestMethod]
+        public void TestTerraformingWithWoods()
+        {
+            var zoneInfoGrid = InitializeZoneInfoGrid(withHorizontalRiver: false, withVerticalRiver: false, withWoodlands: true);
+
+            Assert.IsFalse(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is EmptyZoneConsumption));
+            Assert.IsTrue(zoneInfoGrid.ZoneInfos.Any(x => x.Value.ConsumptionState.GetZoneConsumption() is WoodlandZoneConsumption));
+            Assert.IsFalse(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is WaterZoneConsumption));
+        }
+
+        [TestMethod]
+        public void TestTerraformingWithRivers()
+        {
+            foreach (var boolPair in new[] { new { A = false, B = true }, new { A = true, B = false } })
+            {
+                var zoneInfoGrid = InitializeZoneInfoGrid(withHorizontalRiver: boolPair.A, withVerticalRiver: boolPair.B, withWoodlands: false);
+
+                Assert.IsFalse(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is EmptyZoneConsumption));
+                Assert.IsFalse(zoneInfoGrid.ZoneInfos.All(x => x.Value.ConsumptionState.GetZoneConsumption() is WoodlandZoneConsumption));
+                Assert.IsTrue(zoneInfoGrid.ZoneInfos.Any(x => x.Value.ConsumptionState.GetZoneConsumption() is WaterZoneConsumption));
+            }
+        }
+    }
+}
