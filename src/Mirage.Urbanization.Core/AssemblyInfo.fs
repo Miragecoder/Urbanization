f8a75266c2e30607407f8d489a79ae3c16acb49b
Rob Wijkstra <rob.wijkstra@gmail.com>
2017-06-18 13:51:16 +0200

    Init experimental F-sharp branch

---
+++ src/Mirage.Urbanization.Core/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Mirage.Urbanization.Core.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Mirage.Urbanization.Core")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Mirage.Urbanization.Core")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("506cacfe-82e8-4d01-a331-d27c4523f81b")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/Mirage.Urbanization.Core/Main.fs
@@ -0,0 +1,6 @@
+﻿namespace Mirage.Urbanization
+open System
+type LogEventArgs(logMessage) =
+   inherit EventArgs() 
+   member val LogMessage = logMessage with get
+   member val CreatedOn = DateTime.Now with get
